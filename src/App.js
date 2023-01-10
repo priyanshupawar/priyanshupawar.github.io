434503b28f4ce523c522161db5287b5487b47ce5
priyanshupawar <talk20priyanshu@gmail.com>
2023-01-10 21:51:07 +0530

    n

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
-
 import Projects from "./Components/Projects";
 import TechStack from "./Components/TechStack";
-// import ProjectCard from "./Components/ProjectCard";
-// import Statistics from "./Components/Statistics";
-// import TechStack from "./Components/TechStack";
-// import Contact from "./Components/Contact";
 
 function App() {
   return (
